dfbc5c580bfb09702e84f64a86d55d1044921253
dung12346 <48148631+dung12346@users.noreply.github.com>
2025-05-28 14:00:03 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Chào mừng đến với Flask trên Render!"
+def hello():
+    return "Flask app running on Render!"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
